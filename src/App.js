d18782fbc34464cd4bd22261ec712f67bd97c656
kamya <kamya.shethia@girlswhocode.com>
2019-12-29 09:43:26 -0500

    remove favicon, maybe render simple card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 
+
+function Card(props) {
+  return <div>Hello, {props.value}</div>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <p>
-		Hello world
-	 </p>
+         <Card
+          value="Sample Card"
+         >
+
+	       </Card>
       </header>
     </div>
   );
